a671580ee0c3eee26e23092064f694dcf2387dca
martnovdmytro <dmytro_martynov@icloud.com>
2022-07-06 12:45:21 +0300

    first template

---
+++ resources/views/main.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+    <main>
+        <section class="container section-category">
+            <div class="img-fluid relative h-4/6 top-14">
+                <img src="/img/macbook.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+                <img src="/img/camera.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+                <img src="/img/tablet.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+                <img src="/img/desctop.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+                <img src="/img/accessories.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+                <img src="/img/phone.jpg" class="col-md-4 float-md-start mx-auto p-0.5"/>
+            </div>
+        </section>
+        <section class="section-product">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 flex-column mt-2 mb-2">
+                        <img class="img-fluid mx-auto p-2" src="https://loremflickr.com/320/240" />
+                        <div class="text-center h-20 font-semibold">
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsam!</a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 flex-column mt-2 mb-2">
+                        <img class="img-fluid mx-auto p-2" src="https://loremflickr.com/320/240" />
+                        <div class="text-center h-20 font-semibold">
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsam!</a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 flex-column mt-2 mb-2">
+                        <img class="img-fluid mx-auto p-2" src="https://loremflickr.com/320/240" />
+                        <div class="text-center h-20 font-semibold">
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsam!</a>
+                        </div>
+                    </div>
+                    <div class="col-md-3 flex-column mt-2 mb-2">
+                        <img class="img-fluid mx-auto p-2" src="https://loremflickr.com/320/240" />
+                        <div class="text-center h-20 font-semibold">
+                            <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, ipsam!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</x-layout>
