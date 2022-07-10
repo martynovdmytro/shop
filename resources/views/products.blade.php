7c23447c4eecb3d9ac168aae5acd7a1eb357e9c2
martnovdmytro <dmytro_martynov@icloud.com>
2022-07-10 16:42:57 +0300

    facade side

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,16 +3,21 @@
         <div class="container">
             <div class="row">
             @foreach($products as $product)
-                    <div class="position-relative col-md-3 flex-column mt-2 mb-2">
+                    <div class="col-md-3 flex-column mt-2 mb-2">
                         <a href="/product/{{ $product->slug }}">
-                        <div class="position-absolute img w-100 h-100 rounded">
-                        </div>
-                            <img class="img-fluid img-thumbnail mx-auto p-2" src="{{ $product->image }}" />
+                            <div class="position-relative">
+                                <div class="position-absolute img w-100 h-100 rounded">
+                                </div>
+                                <img class="img-fluid img-thumbnail mx-auto p-2" src="{{ $product->image }}" />
+                            </div>
                         </a>
-                        <div class="img-thumbnail mt-2 p-3 h-40 font-semibold">
+                        <div class="img-thumbnail mt-2 p-3 h-40 font-semibold text-truncate">
                             <h5><a class="nav-link" href="/product/{{ $product->slug }}">{{ $product->title }}</a></h5>
                             <p>{{ $product->description }}</p>
                             <p>{{ $product->price }}$</p>
+                            <div class="row">
+                                <button class="btn btn-danger col-md-10 mx-auto mt-2">Add to card</button>
+                            </div>
                         </div>
                     </div>
                 @endforeach
