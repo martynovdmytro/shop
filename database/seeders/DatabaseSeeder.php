0aeb630c6dd263765d9e1ae163c8e708193fb90d
martnovdmytro <dmytro_martynov@icloud.com>
2022-07-14 07:23:20 +0300

    admin auth added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Attribute;
-use App\Models\AttributeDescription;
 use App\Models\Category;
 use App\Models\Description;
 use App\Models\Feature;
 use App\Models\Product;
-use App\Models\ProductAttribute;
-use App\Models\User;
-use Database\Factories\AttributeFactory;
-use Database\Factories\DescriptionFactory;
-use Database\Factories\ProductAttributeFactory;
-use Database\Factories\ProductFactory;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use PhpParser\Node\Expr\Cast\Int_;
 
 class DatabaseSeeder extends Seeder
 {
